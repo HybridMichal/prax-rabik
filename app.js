cc0ddae3d86e63af04cc9790b0c2f83fb8a9d550
HybridMichal <118260010+HybridMichal@users.noreply.github.com>
2024-01-14 22:23:46 +0100

    ups

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const port = 3000;
 
 app.use(express.static('public'));
 
-// MySQL database connection configuration
 const dbConfig = {
     host: 'localhost',
     user: 'root',
@@ -16,32 +15,26 @@ const dbConfig = {
     database: 'form',
 };
 
-// Nodemailer configuration for sending emails
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'your_email@gmail.com', // replace with your Gmail email address
+        user: '#$#$#$@gmail.com', // replace with your Gmail email address
         pass: 'your_email_password',  // replace with your Gmail password or an application-specific password
     },
 });
 
-// Create a MySQL connection pool
 const pool = mysql.createPool(dbConfig);
 
-// Middleware to parse JSON and urlencoded form data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve HTML file
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-// Handle form submissions
 app.post('/submit', (req, res) => {
     const { name, number, email, subject, message } = req.body;
 
-    // Insert the form data into the MySQL database
     pool.query(
         'INSERT INTO submissions (name, number, email, subject, message) VALUES (?, ?, ?, ?, ?)',
         [name, number, email, subject, message],
@@ -50,7 +43,6 @@ app.post('/submit', (req, res) => {
                 console.error(error);
                 res.status(500).send('Internal Server Error');
             } else {
-                // Send an email
                 sendEmail(name, email, subject, message);
 
                 res.status(200).send('Form submitted successfully!');
